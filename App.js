e9fd36395540cc91047f7c6423668ae351a81fe0
Sonal <sonalkhare.sk215@gmail.com>
2020-10-27 11:43:02 +1100

    commit App and index

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {BrowserRouter,Route, NavLink, Switch} from 'react-router-dom';
+import MainPage from './components/MainPage';
+import Content from './components/Content/Content';
+import classes from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav className={classes.mainNav}>
+            <ul>
+              <li><NavLink to="/" exact activeClassName={classes.linkIsActive}>Home</NavLink></li>
+              <li><NavLink to="/content" exact activeClassName={classes.linkIsActive}>Calendar</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+
+        <Route path="/" exact component={MainPage} />
+        <Route path="/content" exact component={Content} />
+        {/* <Route path="/new-booking" render={props=><NewBooking {...props} loginUser={this.props.user} />} /> */}
+      </div>
+    )
+  }
+  
+}
+
+// export default BrowserRouter(App);
+export default App;
